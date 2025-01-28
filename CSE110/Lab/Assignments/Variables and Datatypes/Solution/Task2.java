5f4e4660b6b75bd2aff60f0f20cc4445b8d5e82a
Ahanaf Chowdhury Nihal <nihalchoww@gmail.com>
2025-01-28 07:50:11 +0600

    coing changes
    assignment from CSE110

---
+++ CSE110/Lab/Assignments/Variables and Datatypes/Solution/Task2.java
@@ -0,0 +1,28 @@
+public class Task2 {
+    public static void main (String [] args){
+        int vari1;
+        vari1 = 3;
+        System.out.println(vari1);
+        int vari2 = 5;
+        System.out.println(vari1 + vari2);
+        System.out.println(vari1 * vari2);
+        System.out.println(vari1 / vari2);
+        double variable1;
+        variable1 = 4.00;
+        System.out.println(variable1);
+        double variable2 = 5.00;
+        System.out.println(variable1 + variable2);
+        System.out.println(variable1 * variable2);
+        System.out.println(variable1 / variable2);
+        int vari3;
+        vari3 = 6;
+        System.out.println(vari3);
+        double variable4 = 5;
+        System.out.println(vari3 + variable4);
+        System.out.println(vari3 * variable4);
+        System.out.println(vari3 / variable4);
+
+
+
+    }
+}
