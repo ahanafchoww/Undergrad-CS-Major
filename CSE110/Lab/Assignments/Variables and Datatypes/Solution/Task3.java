014aec46350b6743dcd7fe3cd20451b596d934a4
Ahanaf Chowdhury Nihal <nihalchoww@gmail.com>
2025-01-29 18:03:12 +0600

    Progress to the assignment

--- CSE110/Lab/Assignments/Variables and Datatypes/Solution/Task3.java
+++ CSE110/Lab/Assignments/Variables and Datatypes/Solution/Task3.java
@@ -1,5 +1,17 @@
 public class Task3 {
     public static void main (String [] args){
-        
+        double pi = 3.1416;
+        int rad = 4;
+        double circum = 2*pi*rad;
+        double area = pi*rad*rad;
+        System.out.println ( " Circumference of the circle is " + circum + " units");
+        System.out.println ( " Area of the circle is " + area + " square units");
+        //we can also use Math.Pi; but it might not be allowed in thid course
+        circum = 2*Math.PI*rad;
+        area = Math.PI*rad*rad;
+        System.out.println ( " Circumference of the circle is " + circum + " units");
+        System.out.println ( " Area of the circle is " + area + " square units");
+
+
     }
 }
