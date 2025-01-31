09253c77db1a3d0b32c1dcb537a6875be42a16de
Ahanaf Chowdhury Nihal <nihalchoww@gmail.com>
2025-01-31 09:33:46 +0600

    Progress

---
+++ CSE110/Lab/Assignments/Variables and Datatypes/Solution/Task6.java
@@ -0,0 +1,25 @@
+public class Task6 {
+    public static void main (String [] args){
+        int firstVari = 34;
+        int secondVari = 10;
+        //First approach
+        int thirdVari;
+        thirdVari = firstVari;
+        firstVari = secondVari;
+        secondVari = thirdVari;
+
+        System.out.println (firstVari);
+        System.out.println (secondVari);
+
+
+
+        //Second approach
+        firstVari = firstVari + secondVari;
+        secondVari = firstVari - secondVari;
+        firstVari = firstVari - secondVari;
+        System.out.println (firstVari);
+        System.out.println (secondVari);
+
+
+    }
+}
