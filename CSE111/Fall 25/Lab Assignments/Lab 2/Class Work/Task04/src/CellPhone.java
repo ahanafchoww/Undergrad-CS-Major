033e8f10e5fa136931354198585f9c3f0bf34fda
Ahanaf Chowdhury Nihal <nihalchoww@gmail.com>
2025-12-24 04:01:20 +0600

    Update

--- CSE111/Fall 25/Lab Assignments/Lab 2/Class Work/Task04/src/CellPhone.java
+++ CSE111/Fall 25/Lab Assignments/Lab 2/Class Work/Task04/src/CellPhone.java
@@ -1,3 +1,32 @@
 public class CellPhone {
-    
+    public String model = "unknown";
+    public int StoredContactsCount = 0;
+    public String [] Contacts = new String[3];
+
+
+    public void storeContact(String a){
+        if (StoredContactsCount<=2){
+            System.out.println ("Contact Stored");
+            Contacts[StoredContactsCount]=a;
+            StoredContactsCount++;
+        }
+        else {
+            System.out.println ("Memory full. New contact can't be stored");
+        }
+    }
+
+    public void printDetails(){
+
+        if (StoredContactsCount==0){
+            System.out.println ("Phone Model " + model + "\n" + "Contacts Stored " + StoredContactsCount);
+        }
+        else {
+            System.out.println ("Phone Model " + model + "\n" + "Contacts Stored " + StoredContactsCount + "\n"
+                    + "Stored Contacts:" );
+            for (int i = 0; i<StoredContactsCount; i++){
+                System.out.println (Contacts[i]);
+            }
+        }
+
+    }
 }
