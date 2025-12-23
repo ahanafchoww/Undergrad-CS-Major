c16d64a15d1f0bcb53a5bd91c658d3447bf2aae3
Ahanaf Chowdhury Nihal <nihalchoww@gmail.com>
2025-12-24 04:21:50 +0600

    Update

--- CSE111/Fall 25/Lab Assignments/Lab 3/ClassWork/Task02/src/Student.java
+++ CSE111/Fall 25/Lab Assignments/Lab 3/ClassWork/Task02/src/Student.java
@@ -1,2 +1,20 @@
 public class Student {
+    public String name;
+    public String prog;
+
+    public Student(String name, String prog){
+       this.name = name;
+       this.prog = prog;
+    }
+
+    public void updateName(String name){
+        this.name = name;
+    }
+    public void updateProgram(String prog){
+        this.prog = prog;
+    }
+
+    public String accessProgram(){
+        return prog;
+    }
 }
