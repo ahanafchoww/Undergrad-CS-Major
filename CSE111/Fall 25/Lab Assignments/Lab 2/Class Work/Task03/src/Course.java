5523b8eaff7693c1d8f38383e68242182045e8f2
Ahanaf Chowdhury Nihal <nihalchoww@gmail.com>
2025-12-24 01:04:57 +0600

    More Update

--- CSE111/Fall 25/Lab Assignments/Lab 2/Class Work/Task03/src/Course.java
+++ CSE111/Fall 25/Lab Assignments/Lab 2/Class Work/Task03/src/Course.java
@@ -1,4 +1,18 @@
 public class Course
 {
+    public String CourseName;
+    public String CourseCode;
+    public int CourseCredit;
 
+
+    public void updateDetails(String CourseName , String CourseCode, int CourseCredit){
+        this.CourseName = CourseName;
+        this.CourseCode = CourseCode;
+        this.CourseCredit = CourseCredit;
+    }
+
+    public void displayCourse(){
+        System.out.println ("Course Name: " + CourseName +  "\n" + "Course Code: " + CourseCode + "\n" +
+                "Course Credit: " + CourseCredit);
+    }
 }
