5c0e448820b40f006dc176b1f1d2b7a4edbbdf66
Ahanaf Chowdhury Nihal <nihalchoww@gmail.com>
2025-12-24 00:16:57 +0600

    Task1 ClassWork Done

--- CSE111/Fall 25/Lab Assignments/Lab 2/Class Work/Task01/src/UniversityTester.java
+++ CSE111/Fall 25/Lab Assignments/Lab 2/Class Work/Task01/src/UniversityTester.java
@@ -1,4 +1,31 @@
 public class UniversityTester
 {
+    public static void main (String[]args){
 
+        /* Task1(a) */
+
+        University U1 = new University(); //First Object
+        University U2 = new University(); //Second Object
+        System.out.println (U1);
+        System.out.println (U2);
+        //No, the location of the objects are not the same here (U1 and U2)
+
+
+
+        /* Task1(b) */
+
+        U1.name = "Imperial College London";
+        U1.country = "England";
+        U2.name = "BRAC University";
+        U2.country = "Bangladesh";
+
+        System.out.println (U1.name);
+        System.out.println (U2.name);
+        // Yes, it has changed - not the same value anymore
+        System.out.println (U1.country);
+        System.out.println (U2.country);
+        // Yes, it has changed - not the same value anymore
+        
+
+    }
 }
