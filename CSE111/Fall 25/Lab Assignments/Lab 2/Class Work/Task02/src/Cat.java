ffe4b55f8dd67bcb2f1bc6ad647c863dc5d86789
Ahanaf Chowdhury Nihal <nihalchoww@gmail.com>
2025-12-24 00:31:07 +0600

    Task2 Classwork Done

--- CSE111/Fall 25/Lab Assignments/Lab 2/Class Work/Task02/src/Cat.java
+++ CSE111/Fall 25/Lab Assignments/Lab 2/Class Work/Task02/src/Cat.java
@@ -1,2 +1,9 @@
 public class Cat {
+    public String color = "White"; //Default Value Setup instead of leaving it null
+    public String action = "sitting"; //Default Value Setup instead of leaving it null
+
+    public void printCat() //Void because it returns nothing while there's no receiving end as well
+    {
+        System.out.println (color + " cat is " + action);
+    }
 }
