391da6cd2f45c82844eca9204a966f1acd1bc825
Ahanaf Chowdhury Nihal <nihalchoww@gmail.com>
2026-01-03 16:51:41 +0600

    Update

---
+++ CSE111/Fall 25/Lab Assignments/Lab 2/Home Work/Task01/src/ImaginaryNumber.java
@@ -0,0 +1,15 @@
+public class ImaginaryNumber {
+    //printNumber() - a string returning method
+    //int realPart
+    //int imaginaryPart
+    // "+" and "i" must be printed separatedly inside the printNumber() method
+
+    public int realPart;
+    public int imaginaryPart;
+
+    public String printNumber(){
+
+        return realPart + " + " + imaginaryPart + "i";
+    }
+
+}
