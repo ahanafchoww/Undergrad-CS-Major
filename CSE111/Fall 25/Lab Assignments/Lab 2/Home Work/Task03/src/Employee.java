7bb7c23383f0e0332396be90ace1af88906503b2
Ahanaf Chowdhury Nihal <nihalchoww@gmail.com>
2026-01-04 15:22:00 +0600

    Update in the System

---
+++ CSE111/Fall 25/Lab Assignments/Lab 2/Home Work/Task03/src/Employee.java
@@ -0,0 +1,18 @@
+public class Employee {
+    // An employee will have a name, salary and designation.
+    // The name will be assigned inside the newEmployee() method
+    // Whenever a New Employee joins his/her salary will be Tk. 30,000 and -
+    // - the designation will be junior.
+    // Employees with salaries greater than Tk. 50,000 and -
+    // - Tk. 30,000 need to pay 30% and 10% of salary as tax respectively.
+    // Employees can be promoted to senior, lead and manager positions.
+    // Based on their promotion they will get an increment of Tk. 25,000, Tk. 50,000 and Tk. 75,000 respectively.
+
+
+    //displayInfo() - void Method
+    //calculateTax() - void Method
+    //promoteEmployee(String) - void Method
+
+    
+
+}
