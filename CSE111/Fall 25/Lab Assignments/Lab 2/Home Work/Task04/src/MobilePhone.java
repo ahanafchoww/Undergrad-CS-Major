2c6fbc403416ae502bffab20c42f148a0ede461a
Ahanaf Chowdhury Nihal <nihalchoww@gmail.com>
2026-01-07 20:47:02 +0600

    update

---
+++ CSE111/Fall 25/Lab Assignments/Lab 2/Home Work/Task04/src/MobilePhone.java
@@ -0,0 +1,7 @@
+public class MobilePhone {
+    //setContactCapacity(int capacity) - void method
+    //details() - void method
+    //addContact(String name, int number) - void method
+    //makeCall(int num) - void method
+    
+}
