19bfb2449dc465dff33f519bacfcf546d814f3e4
Ahanaf Chowdhury Nihal <nihalchoww@gmail.com>
2026-01-12 14:06:45 +0600

    Update

--- CSE111/Fall 25/Lab Assignments/Lab 3/ClassWork/Task04/src/Library.java
+++ CSE111/Fall 25/Lab Assignments/Lab 3/ClassWork/Task04/src/Library.java
@@ -1,2 +1,36 @@
 public class Library {
+    public int capacity;
+    public int bookCount;
+    public String [] bookList;
+
+
+
+    public Library(int capacity){
+        this.capacity = capacity;
+        bookList = new String [capacity];
+        System.out.println("A library has been created with capacity " + this.capacity);
+    }
+
+    public void addBook(String bookName){
+        if (bookCount<capacity){
+                bookList[bookCount] = bookName;
+                System.out.println ("Book '" + bookName + "' added to the Library");
+            bookCount++;
+        }
+        else {
+            System.out.println ("Exceeds maximum capacity. You can't add more than " +
+                    capacity + " books");
+        }
+    }
+
+
+    public void printDetail(){
+        System.out.println("Maximum Capacity: " + capacity);
+        System.out.println("Total Books: " + bookCount);
+        System.out.println("Book List:");
+        for (int i = 0; i<bookCount; i++){
+            System.out.println(bookList[i]);
+        }
+    }
+
 }
