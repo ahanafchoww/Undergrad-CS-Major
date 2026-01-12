ba700274900bcfb0adbbb0130dbc975185ed61df
Ahanaf Chowdhury Nihal <nihalchoww@gmail.com>
2026-01-12 21:17:39 +0600

    Update TaxiLagbe.java

--- CSE111/Fall 25/Lab Assignments/Lab 3/ClassWork/Task05/src/TaxiLagbe.java
+++ CSE111/Fall 25/Lab Assignments/Lab 3/ClassWork/Task05/src/TaxiLagbe.java
@@ -1,2 +1,14 @@
 public class TaxiLagbe {
+    public String plateNumber;
+    public String location;
+
+
+    public TaxiLagbe(String plateNumber, String location){
+        this.plateNumber = plateNumber;
+        this.location = location;
+    }
+
+    public TaxiLagbe(String location){
+        this.location = location;
+    }
 }
