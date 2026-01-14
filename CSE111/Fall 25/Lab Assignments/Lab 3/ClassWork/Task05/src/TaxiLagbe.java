121e983b1006c97f913cff6d804712f1cdd9954f
Ahanaf Chowdhury Nihal <nihalchoww@gmail.com>
2026-01-14 17:22:12 +0600

    Update

--- CSE111/Fall 25/Lab Assignments/Lab 3/ClassWork/Task05/src/TaxiLagbe.java
+++ CSE111/Fall 25/Lab Assignments/Lab 3/ClassWork/Task05/src/TaxiLagbe.java
@@ -1,25 +1,66 @@
+import java.util.Arrays;
 public class TaxiLagbe {
     public String plateNumber;
     public String location;
     public int passengerCount;
-    public String [] passengerList = new String [4];
+    public String[] passengerList = new String[4];
+    public int fareCollected;
 
 
-    public TaxiLagbe(String plateNumber, String location){
+    public TaxiLagbe(String plateNumber, String location) {
+        System.out.println("A new taxi has been created");
         this.plateNumber = plateNumber;
         this.location = location;
     }
 
-    public TaxiLagbe(String plateNumber){
+    public TaxiLagbe(String plateNumber) {
+        System.out.println("A new taxi has been created");
         this.plateNumber = plateNumber;
         this.location = "Khulna";
     }
 
-    public void addPassenger(String passengerName, int fare){
+    public void addPassenger(String passengerName, int fare) {
+        if (passengerCount < 4) {
+            passengerList[passengerCount] = passengerName;
+            System.out.println("Dear " + passengerName + "! Welcome to TaxiLagbe");
+            passengerCount++;
+            fareCollected += fare;
+        } else {
+            System.out.println("Taxi Full! No more passengers can be added");
+        }
 
     }
 
-    public void addPassenger(String passengerName, int fare, String passengerName2, int fare2){
+    public void addPassenger(String passengerName, int fare, String passengerName2, int fare2) {
+        if (passengerCount < 3) {
+            passengerList[passengerCount] = passengerName;
+            System.out.println("Dear " + passengerName + "! Welcome to TaxiLagbe");
+            passengerCount++;
+            fareCollected += fare;
+            passengerList[passengerCount] = passengerName2;
+            System.out.println("Dear " + passengerName2 + "! Welcome to TaxiLagbe");
+            passengerCount++;
+            fareCollected += fare2;
+        } else {
+            System.out.println("Taxi Full! No more passengers can be added");
+        }
+    }
 
+
+    public void printDetails() {
+        System.out.print("Taxi number: " + plateNumber + "\n" +
+                "This taxi can cover " + location + "\n" +
+                "Total Passenger: " + passengerCount + "\n");
+
+        System.out.println("Passenger List:");
+
+        for (int i = 0; i < passengerCount; i++) {
+            System.out.print((passengerList[i]) + " ");
+        }
+
+        if (passengerCount!=0){
+            System.out.println();
+        }
+            System.out.println ("Total Collected fair: " + fareCollected + " Taka");
     }
 }
