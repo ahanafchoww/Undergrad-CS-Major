4100e149b952e628525abbd7bc1f6364f0531019
Ahanaf Chowdhury Nihal <nihalchoww@gmail.com>
2026-01-16 20:28:24 +0600

    Update Reader.java

--- CSE111/Fall 25/Lab Assignments/Lab 3/HomeWork/Task03/src/Reader.java
+++ CSE111/Fall 25/Lab Assignments/Lab 3/HomeWork/Task03/src/Reader.java
@@ -1,2 +1,42 @@
 public class Reader {
+
+
+    public int capacity = 2;
+    public String name = "New user";
+    public int bookCounter;
+    public String [] bookList;
+
+    public Reader (String name, int capacity){
+        this.name = name;
+        this.capacity = capacity;
+        System.out.println("A new reader is created!");
+        bookList = new String [capacity];
+    }
+
+    public Reader (String name){
+        this.name = name;
+        System.out.println("A new reader is created!");
+        bookList = new String [capacity];
+    }
+
+    public void addBook(String bookName){
+        if (bookCounter<capacity){
+            bookList[bookCounter] = bookName;
+            bookCounter++;
+        }
+        else {
+            System.out.println("No more capacity");
+        }
+    }
+
+    public void readerInfo(){
+
+    }
+
+
+    public void updateCapacity(int newCapacity)
+    {
+        this.capacity = newCapacity;
+        System.out.println("Capacity has changed to " + newCapacity);
+    }
 }
