aab6d526ae4c67618d83f4bb520c730e395ca744
Ahanaf Chowdhury Nihal <nihalchoww@gmail.com>
2026-01-16 22:14:08 +0600

    Update MoneyTracker.java

--- CSE111/Fall 25/Lab Assignments/Lab 3/HomeWork/Task04/src/MoneyTracker.java
+++ CSE111/Fall 25/Lab Assignments/Lab 3/HomeWork/Task04/src/MoneyTracker.java
@@ -10,4 +10,18 @@ public class MoneyTracker{
     public MoneyTracker(){
         System.out.println("A new money tracker has been launched.");
     }
+
+    public void createTracker(String name){
+        this.name = name;
+        balance = 1.0;
+    }
+    public void income(int income){
+        balance += (income * 1.0);
+        System.out.println("Balance Updated");
+    }
+
+    public void expense(int expense){
+        balance -= (expense*1.0);
+        System.out.println("Balance Updated");
+    }
 }
