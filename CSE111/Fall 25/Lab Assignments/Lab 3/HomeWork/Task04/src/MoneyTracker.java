6641c85fd6c0519f3ef00f1dbc8bbadd94e5c453
Ahanaf Chowdhury Nihal <nihalchoww@gmail.com>
2026-01-17 21:39:42 +0600

    Update

--- CSE111/Fall 25/Lab Assignments/Lab 3/HomeWork/Task04/src/MoneyTracker.java
+++ CSE111/Fall 25/Lab Assignments/Lab 3/HomeWork/Task04/src/MoneyTracker.java
@@ -1,44 +1,46 @@
-public class MoneyTracker{
+public class MoneyTracker {
     public String name;
     public double balance;
     public double lastSpent;
     public double lastAdded;
 
-
-    public String info(){
-        return "Name: " + name + "\n" + "Current Balance: " + balance;
+    public MoneyTracker() {
+        System.out.println("A new money tracker has been launched. ");
     }
 
-    public MoneyTracker(){
-        System.out.println("A new money tracker has been launched.");
-    }
-
-    public void createTracker(String name){
+    public void createTracker(String name) {
         this.name = name;
         balance = 1.0;
     }
-    public void income(int income){
-        balance += (income * 1.0);
-        System.out.println("Balance Updated");
+
+    public String info() {
+        return "Name: " + name + "\n" +
+                "Current Balance: " + balance;
+    }
+
+    public void income(int income) {
+        balance += income * 1.0;
+        System.out.println("Balance Updated!");
         lastAdded = income;
     }
 
-    public void expense(int expense){
-        if (balance>expense) {
-            balance -= (expense * 1.0);
-            System.out.println("Balance Updated");
+    public void expense(int expense) {
+        if (balance > expense) {
+            balance -= expense * 1.0;
+            System.out.println("Balance Updated.");
             lastSpent = expense;
         }
-        else if (balance<expense){
+        else if (balance < expense) {
             System.out.println("Not enough balance.");
         }
         else {
             System.out.println("You're broke!");
+            lastSpent = expense;
         }
     }
 
-    public void showHistory(){
-        System.out.println ("Last added: " + lastAdded + "\n" +
-                "Last spent: " + lastSpent);
+    public void showHistory() {
+        System.out.println("Last added: " + lastAdded);
+        System.out.println("Last spent: " + lastSpent);
     }
 }
