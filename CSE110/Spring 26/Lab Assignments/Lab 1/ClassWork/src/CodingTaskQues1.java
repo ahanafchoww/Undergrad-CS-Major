5b15c0a0f11c31c7c7a36511fe0af12b17132964
Ahanaf Chowdhury Nihal <nihalchoww@gmail.com>
2026-05-16 15:57:29 +0600

    Update

--- CSE110/Spring 26/Lab Assignments/Lab 1/ClassWork/src/CodingTaskQues1.java
+++ CSE110/Spring 26/Lab Assignments/Lab 1/ClassWork/src/CodingTaskQues1.java
@@ -1,64 +1,71 @@
 public class CodingTaskQues1 {
     public static void main(String[] args) {
 
-        //(a)
+        // (a) LEGAL
         int hungry;
-        // legal
+        // A simple name consisting only of lowercase letters.
 
-        //(b)
+
+        // (b) ILLEGAL
         // int 2AB;
-        // illegal - can not start with a numeric digit.
+        // Explanation: Java identifiers can contain numbers, but they cannot START with a number.
 
 
-        //(c)
+        // (c) ILLEGAL
         // int 312.2;
-        // illegal - can not start with a numeric digit.
+        // Explanation: It starts with a number, which is illegal. Furthermore, it contains a
+        // period (.), which Java uses as an operator to access methods and variables, not as a name character.
 
 
-        //(d)
+        // (d) LEGAL
         String MOBILE;
-        // legal
+        // Consists entirely of upper-case letters, which are perfectly valid characters.
 
 
-        //(e)
+        // (e) ILLEGAL
         // int "Ans";
-        // illegal - can not contain special characters other than underscore (_) and $
-        // also, when the first double quotation mark hits, it starts being taken as String's data value
+        // Explanation: It contains double quotation marks. In Java, anything in double quotes
+        // is treated as literal String data, not a variable name.
 
 
-        //(f)
+        // (f) LEGAL
         String $30;
+        // The dollar sign ($) is one of the few special characters completely allowed anywhere in a Java name.
 
 
-        //(g)
+        // (g) ILLEGAL
         // int Yes/No;
-        //illegal - can not contain special characters other than underscore (_) and $
+        // Explanation: It contains a forward slash (/). Java views this as the division operator,
+        // so the compiler thinks you are trying to divide a variable named 'Yes' by a variable named 'No'.
 
 
-        //(h)
+        // (h) ILLEGAL
         // int student-id;
-        // illegal - can not contain special characters other than underscore (_) and $
+        // Explanation: It contains a hyphen (-). Java views this as the subtraction operator,
+        // so the compiler thinks you are trying to subtract 'id' from 'student'.
 
 
-        //(i)
+        // (i) ILLEGAL
         // double A+3;
-        // illegal - can not contain special characters other than underscore (_) and $
+        // Explanation: It contains a plus sign (+). Java views this as the addition operator,
+        // so the compiler thinks you are trying to add 3 to a variable named 'A'.
 
 
-        //(j)
+        // (j) ILLEGAL
         // int 'X';
-        // illegal - can not contain special characters other than underscore (_) and $
+        // Explanation: It contains single quotation marks. In Java, single quotes are used
+        // exclusively to define a character literal (char), not a variable name.
 
 
-        //(k)
+        // (k) ILLEGAL
         // int public;
-        //illegal - can not use Java Keywords
+        // Explanation: 'public' is a reserved keyword in Java. These keywords have predefined
+        // meanings built into the language, so you cannot use them as your own variable names.
 
 
-        //(l)
+        // (l) LEGAL
         int student_id;
-
+        // Letters and underscores (_) are completely legal characters.
 
     }
-
 }
