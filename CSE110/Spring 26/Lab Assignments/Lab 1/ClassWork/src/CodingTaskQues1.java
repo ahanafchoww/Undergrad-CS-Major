7924e885010ece895b89dffee98dde683fc6caed
Ahanaf Chowdhury Nihal <nihalchoww@gmail.com>
2026-05-16 11:44:26 +0600

    Update

---
+++ CSE110/Spring 26/Lab Assignments/Lab 1/ClassWork/src/CodingTaskQues1.java
@@ -0,0 +1,30 @@
+public class CodingTaskQues1 {
+    public static void main (String [] args){
+
+        //(a)
+        int hungry;
+        // legal
+
+        //(b)
+        // int 2AB;
+        // illegal - can not start with a numeric digit.
+
+
+        //(c)
+        // int 312.2;
+        // illegal - can not start with a numeric digit.
+
+
+        //(d)
+        String MOBILE;
+        // legal
+
+
+        //(e)
+        // int "Ans";
+        // illegal -
+
+
+
+    }
+}
