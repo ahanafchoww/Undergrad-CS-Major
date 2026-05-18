1ed07498aabe96b4579030416c1cdda77cd0ce18
Ahanaf Chowdhury Nihal <nihalchoww@gmail.com>
2026-05-18 22:51:20 +0600

    Update CodingTaskQues2.java

--- CSE110/Spring 26/Lab Assignments/Lab 1/ClassWork/src/CodingTaskQues2.java
+++ CSE110/Spring 26/Lab Assignments/Lab 1/ClassWork/src/CodingTaskQues2.java
@@ -1,6 +1,26 @@
 public class CodingTaskQues2 {
     public static void main (String [] args){
+        //(1)
+        int vari1;
+        vari1 = 2;
+        System.out.println (vari1);
 
-        
+        //(2)
+        int vari2;
+        vari2 = 5;
+        int addition = vari1 + vari2;
+        System.out.println (addition);
+
+        //(3)
+        int product = vari1 * vari2;
+        System.out.println (product);
+        int division = vari2 / vari1;
+        System.out.println (division);
+
+        //(4)
+        double vari3;
+        vari3 = 2;
+        System.out.println (vari3);
+
     }
 }
